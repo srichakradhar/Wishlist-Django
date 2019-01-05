fa1dfc899df121fc36eb14f0c2a45a17af6d202f
srichakradhar <srichakra3nsr3@gmail.com>
2019-01-05 21:41:50 +0530

    Add new views - class based - List and RUD

--- wishes/urls.py
+++ wishes/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url
 from wishes import views
 
 urlpatterns = [
-    url(r'^wishes/$',views.wish_list),
-    url(r'^wishes/(?P<pk>[0-9]+)/$',views.wish_detail),
+    # url(r'^wishes/$',views.wish_list),
+    # url(r'^wishes/(?P<pk>[0-9]+)/$',views.wish_detail),
+    url(r'^wishes/$',views.WishList.as_view()),
+    url(r'^wishes/(?P<pk>[0-9]+)/$',views.WishDetail.as_view()),
 ]
--- wishes/views.py
+++ wishes/views.py
@@ -7,48 +7,12 @@ from rest_framework.parsers import JSONParser
 from rest_framework.renderers import JSONRenderer
 from wishes.models import Wish
 from wishes.serializers import WishSerializer
-from django.utils.six import BytesIO
+from rest_framework import generics
 
-@csrf_exempt
-def wish_list(request):
-    """
-    List all wishes or create a new wish
-    """
-    if request.method == 'GET':
-        wishes = Wish.objects.all()
-        serializer = WishSerializer(wishes, many=True)
-        return JsonResponse(serializer.data, safe=False)
+class WishList(generics.ListCreateAPIView):
+    queryset = Wish.objects.all()
+    serializer_class = WishSerializer
 
-    if request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = WishSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
-
-@csrf_exempt
-def wish_detail(request,pk):
-    """
-    Retrieve, update or delete a birthday wish.
-    """
-    try:
-        wish = Wish.objects.get(pk=pk)
-    except Wish.DoesNotExist:
-        return HttpResponse(status=404)
-
-    if request.method == 'GET':
-        serializer = WishSerializer(wish)
-        return JsonResponse(serializer.data)
-
-    elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = WishSerializer(wish, data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
-
-    elif request.method == 'DELETE':
-        wish.delete()
-        return HttpResponse(status=204)
+class WishDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Wish.objects.all()
+    serializer_class = WishSerializer
